d93dcfcf4261ec2ef92b9dc571694598fae09ccb
Евгений Шкумат <kotbmasle@gmail.com>
2024-02-25 07:43:08 +0300

    add and use ranom_init function

--- Project01/main.cpp
+++ Project01/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 using namespace std;
 
+void random_init(int* array, int size, int start);
+string output(int* array, int size);
+
 int main() {
+	int size = 10;
+	int* vector = new int[size];
+
+	random_init(vector, size, 1);
+
+	cout << "Before:\n" << output(vector, size);
 
 	return 0;
 }
+
