ec42af8c5aa7736022d5460d1ff056045c54857b
Евгений Шкумат <kotbmasle@gmail.com>
2024-02-25 07:50:30 +0300

    final version

--- Project01/main.cpp
+++ Project01/main.cpp
@@ -4,6 +4,10 @@ using namespace std;
 void random_init(int* array, int size, int start);
 string output(int* array, int size);
 
+int find_max_element(int* array, int size);
+int find_min_element(int* array, int size);
+void swap_s(int* a, int* b);
+
 int main() {
 	int size = 10;
 	int* vector = new int[size];
@@ -11,6 +15,13 @@ int main() {
 	random_init(vector, size, 1);
 
 	cout << "Before:\n" << output(vector, size);
+
+	int min = find_min_element(vector, size);
+	int max = find_max_element(vector, size);
+
+	swap_s((vector + max), (vector + min));
+
+	cout << "\nAfter:\n" << output(vector, size);
 
 	return 0;
 }
